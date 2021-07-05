c12af421b0866ddd3e6a575303d9d75668367665
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-07-05 19:12:23 -0400

    nar_earnings

--- nar_earnings/plots.js
+++ nar_earnings/plots.js
@@ -1,23 +1,24 @@
 var apiKey = "NABDZFV4HO4HQ68G"
 
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=${apiKey}';
+var url = 'https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=NABDZFV4HO4HQ68G';
 
 console.log(url)
 
-var request = require('request');
+function EARNINGS(rows, index) {
+  return rows.map(function(row) {
+    return row[index]
+  })
+}
 
-request.get({
-    url: url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is successfully parsed as a JSON object:
-      console.log(data);
-    }
-});
+function buildPlot() {
+  d3.json(url).then(result =>{
+    console.log(result)
+    var name = result.dataset.name;
+    var data = result.dataset.data;
+
+    var dates = unpack(data, 0);
+    console.log(datas)
+  })
+}
+
